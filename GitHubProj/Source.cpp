f73997c49938875e3acad5d27c810897efd8cf9b
goosee452 <102236719+goosee452@users.noreply.github.com>
2022-03-28 18:00:33 +0300

    Update Source.cpp

--- GitHubProj/Source.cpp
+++ GitHubProj/Source.cpp
@@ -4,11 +4,13 @@
 int main() {
 
 
-	int a, b, c;
-	cin >> a >> b >> c;
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
     
 	int max = a > b ? a : b;
 	max > c ? max : max = c;
+	max > d ? max : max = d;
+	
 	cout << max;
 
 	return 0;
